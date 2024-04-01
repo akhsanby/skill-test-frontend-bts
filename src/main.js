252fa16ab2b34ab5e3ac4f6b61245809ba406bc4
akhsanby <akhsanby@gmail.com>
2024-04-01 16:49:57 +0700

    config vue toastify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import Vue3Toasity from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(Vue3Toasity, {
+    autoClose: 3000,
+  })
+  .mount("#app");
