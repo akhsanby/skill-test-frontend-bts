2b27808573e690c7040993c126c63c0c22849484
akhsanby <akhsanby@gmail.com>
2024-04-01 15:24:24 +0700

    initial home view

---
+++ src/view/Home.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+<template>
+  <div class="card w-96 bg-base-100 shadow border">
+    <div class="card-body">
+      <h2 class="card-title">Checklist App</h2>
+      <div class="card bg-base-100 shadow border">
+        <div class="card-body p-4">
+          <p>1</p>
+        </div>
+      </div>
+      <div class="card bg-base-100 shadow border">
+        <div class="card-body p-4">
+          <p>2</p>
+        </div>
+      </div>
+      <div class="card bg-base-100 shadow border">
+        <div class="card-body p-4">
+          <p>3</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
